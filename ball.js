1e7aaf1fdb17d9cef06f5458401c77ebdeacc9cf
NachoAJ <narchercher@gmail.com>
2019-03-14 10:28:20 +0100

    Código bonito

--- ball.js
+++ ball.js
@@ -1,30 +1,30 @@
-function Ball(){
+function Ball() {
 
-  this.x = width/2;
+  this.x = width / 2;
   this.y = 50;
-  this. velocity = 0;
+  this.velocity = 0;
   this.gravity = 0;
   this.lift = -15;
   this.velocityx = 0;
 
 
-  this.show = function(){
-  fill(255);
-  ellipse(this.x,this.y,20,20);
+  this.show = function() {
+    fill(255);
+    ellipse(this.x, this.y, 20, 20);
   }
 
   this.up = function() {
     this.velocity = this.lift;
   }
 
-  this.start = function(){
+  this.start = function() {
     this.gravity = 0.5;
   }
 
   this.reset = function() {
-    this.x = width/2;
+    this.x = width / 2;
     this.y = 50;
-    this. velocity = 0;
+    this.velocity = 0;
     this.gravity = 0.5;
     this.lift = -15;
     this.velocityx = 0;
@@ -32,15 +32,15 @@ function Ball(){
   }
 
   this.bounce = function() {
-    if(this.x < platform.x+50){
-      this.velocityx += -(this.x - platform.x + 50)*0.02;
-      if(this.velocityx > 0){
+    if (this.x < platform.x + 50) {
+      this.velocityx += -(this.x - platform.x + 50) * 0.02;
+      if (this.velocityx > 0) {
         this.velocityx *= -1;
       }
     }
-    if(this.x >= platform.x+50){
-      this.velocityx += (this.x - platform.x + 50)*0.02;
-      if(this.velocityx < 0){
+    if (this.x >= platform.x + 50) {
+      this.velocityx += (this.x - platform.x + 50) * 0.02;
+      if (this.velocityx < 0) {
         this.velocityx *= -1;
       }
     }
@@ -48,16 +48,16 @@ function Ball(){
 
   }
 
-  this.update = function(){
+  this.update = function() {
     this.velocity += this.gravity;
     this.y += this.velocity;
     this.x += this.velocityx;
-    if (this.y > height){
+    if (this.y > height) {
       this.y = height;
       this.velocity = 0;
       this.velocityx = 0;
     }
-    if (this.y < 0){
+    if (this.y < 0) {
       this.y = 0;
       this.velocity = 0;
     }
--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 function setup() {
-  createCanvas(1000,400);
+  createCanvas(1000, 400);
   ball = new Ball();
   platform = new Platform();
   noCursor();
@@ -16,26 +16,26 @@ function draw() {
 
   var i;
 
-  for( i = 0; i <= width; i+=10){
+  for (i = 0; i <= width; i += 10) {
     fill(0);
-    triangle(i,height,i+5,height-15,i+10,height);
+    triangle(i, height, i + 5, height - 15, i + 10, height);
 
   }
 
-  if(ball.gravity == 0){
+  if (ball.gravity == 0) {
     textSize(35);
-    fill(255,0,0);
-    text('Haz click para empezar',(width/2)-200,height/2);
-    if (mouseIsPressed){
-          ball.start();
+    fill(255, 0, 0);
+    text('Haz click para empezar', (width / 2) - 200, height / 2);
+    if (mouseIsPressed) {
+      ball.start();
     }
   }
 
 
-  if (platform.hits(ball)){
+  if (platform.hits(ball)) {
     ball.up();
     platform.count += 1;
-    if (platform.record < platform.count){
+    if (platform.record < platform.count) {
       platform.record = platform.count;
     }
     print("hit");
@@ -43,12 +43,12 @@ function draw() {
   }
 
   if (ball.y == height) {
-    fill(255,0,0);
+    fill(255, 0, 0);
     textSize(100);
-    text('GAME OVER',(width/2)-300,height/2);
+    text('GAME OVER', (width / 2) - 300, height / 2);
     textSize(25);
-    text('Haz click para reiniciar',(width/2)-135,(height/2)+50);
-    if (mouseIsPressed){
+    text('Haz click para reiniciar', (width / 2) - 135, (height / 2) + 50);
+    if (mouseIsPressed) {
       ball.reset();
     }
   }
